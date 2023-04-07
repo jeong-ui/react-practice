d4cd298edd45a56131d30f4158def97604bdbbf5
Uiho <lopica94@gmail.com>
2023-04-07 16:10:00 +0900

    uncontrolled input

--- react-practice/src/practice/wordRelay/WordRelay.jsx
+++ react-practice/src/practice/wordRelay/WordRelay.jsx
@@ -4,13 +4,14 @@ const { useState, useRef } = React;
 const WordRelay = () =>  {
     const [word, setWord] = useState('시작');
     const [result , setResult] = useState(''); 
-    const [value , setValue] = useState('');
+    // const [value , setValue] = useState('');
     
     const inputRef = useRef(null);
 
     const onSubmit = (e) => {
         e.preventDefault();
 
+        let value = e.target.children.word.value;
         if(word.slice(-1) === value.slice(0,1)){
             setResult('OK');
             setWord(value);
@@ -19,20 +20,23 @@ const WordRelay = () =>  {
             setResult('No');
         }
 
-        setValue('');
+        //setValue('');
+        value = '';
         inputRef.current.focus();
     };
 
-    const onChange = (e) => {
-        setValue(e.target.value);
-    };
+    // const onChange = (e) => {
+    //     setValue(e.target.value);
+    // };
 
     return(
         <>
             <div>{word}</div>
             <form onSubmit={onSubmit}>
-                <input ref = {inputRef} type="text" value ={value} onChange={onChange}/>
-                <button> submit </button>
+                {/* <input ref = {inputRef} type="text" value ={value} onChange={onChange}/>  */}
+                {/* uncontrolled Input 으로 수정 */}
+                <input defaultValue="끝말잇기" id ="word" ref ={inputRef}></input>
+                <button> submit !!</button>
             </form>
             <div>{result}</div>
     
